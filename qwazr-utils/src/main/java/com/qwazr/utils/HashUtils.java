0f8bb77596c825264bb5fa5be117fd833aeb2977
Emmanuel Keller <ekeller@open-search-server.com>
2015-07-05 22:14:13 +0200

    Add md5

--- qwazr-utils/src/main/java/com/qwazr/utils/HashUtils.java
+++ qwazr-utils/src/main/java/com/qwazr/utils/HashUtils.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,11 +17,31 @@ package com.qwazr.utils;
 
 import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class HashUtils {
 
 	public static final int getMurmur3Mod(final String hashString, final int mod) {
 		HashFunction m3 = Hashing.murmur3_128();
 		return (Math.abs(m3.hashString(hashString).asInt()) % mod);
+	}
+
+	public static String md5Hex(File file) throws IOException {
+		FileInputStream fis = new FileInputStream(file);
+		try {
+			BufferedInputStream bis = new BufferedInputStream(fis);
+			try {
+				return DigestUtils.md5Hex(bis);
+			} finally {
+				IOUtils.closeQuietly(bis);
+			}
+		} finally {
+			IOUtils.closeQuietly(fis);
+		}
 	}
 }
