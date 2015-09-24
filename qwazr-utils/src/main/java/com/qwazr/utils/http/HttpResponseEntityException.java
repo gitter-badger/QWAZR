13f86d2a9fb45ae2249a8a174b111eed3e5884ea
Emmanuel Keller <ekeller@open-search-server.com>
2015-09-24 22:59:52 +0200

    Better error report

--- qwazr-utils/src/main/java/com/qwazr/utils/http/HttpResponseEntityException.java
+++ qwazr-utils/src/main/java/com/qwazr/utils/http/HttpResponseEntityException.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2014-2015 Emmanuel Keller / QWAZR
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,42 +31,53 @@ import org.apache.http.client.HttpResponseException;
 
 public class HttpResponseEntityException extends HttpResponseException {
 
-	private static final long serialVersionUID = 1958648159987063347L;
+    private static final long serialVersionUID = 1958648159987063347L;
 
-	private final String contentType;
-	private final String contentMessage;
+    private final String contentType;
+    private final String contentMessage;
 
-	public HttpResponseEntityException(HttpResponse response, String message) {
-		super(getStatusCode(response.getStatusLine()), message);
-		HttpEntity entity = response.getEntity();
-		String cm = null;
-		String ct = null;
-		if (entity != null) {
-			try {
-				cm = IOUtils.toString(entity.getContent());
-				Header header = entity.getContentType();
-				if (header != null)
-					ct = header.getValue();
-			} catch (IllegalStateException | IOException e) {
-			}
-		}
-		contentMessage = cm == null ? message : cm;
-		contentType = ct == null ? MediaType.TEXT_PLAIN : ct;
+    public HttpResponseEntityException(HttpResponse response, String message) {
+	super(getStatusCode(response.getStatusLine()), message);
+	HttpEntity entity = response.getEntity();
+	String cm = null;
+	String ct = null;
+	if (entity != null) {
+	    try {
+		cm = IOUtils.toString(entity.getContent());
+		Header header = entity.getContentType();
+		if (header != null)
+		    ct = header.getValue();
+	    } catch (IllegalStateException | IOException e) {
+	    }
 	}
+	contentMessage = cm == null ? message : cm;
+	contentType = ct == null ? MediaType.TEXT_PLAIN : ct;
+    }
 
-	private static int getStatusCode(StatusLine statusLine) {
-		if (statusLine == null)
-			return 0;
-		return statusLine.getStatusCode();
-	}
+    private static int getStatusCode(StatusLine statusLine) {
+	if (statusLine == null)
+	    return 0;
+	return statusLine.getStatusCode();
+    }
 
-	public WebApplicationException getWebApplicationException() {
-		int code = getStatusCode();
-		if (code == 0)
-			return new WebApplicationException(this);
-		ResponseBuilder response = Response.status(code);
-		if (contentMessage != null)
-			response.type(contentType).entity(contentMessage);
-		return new WebApplicationException(this, response.build());
-	}
+    public WebApplicationException getWebApplicationException() {
+	int code = getStatusCode();
+	if (code == 0)
+	    return new WebApplicationException(this);
+	ResponseBuilder response = Response.status(code);
+	if (contentMessage == null)
+	    return new WebApplicationException(this, response.build());
+	response.type(contentType).entity(contentMessage);
+	return new WebApplicationException(contentMessage, this, response.build());
+
+    }
+
+    public static HttpResponseEntityException findFirstCause(Throwable e) {
+	if (e == null)
+	    return null;
+	if (e instanceof HttpResponseEntityException)
+	    return (HttpResponseEntityException) e;
+	return findFirstCause(e.getCause());
+    }
+
 }
