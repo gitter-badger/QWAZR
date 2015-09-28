61831f9c2a6cad325b1c02cd303905b163b7f8f5
Emmanuel Keller <ekeller@open-search-server.com>
2015-09-28 13:43:05 +0200

    Graph test passed

--- qwazr-graph/src/main/java/com/qwazr/graph/model/GraphNode.java
+++ qwazr-graph/src/main/java/com/qwazr/graph/model/GraphNode.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2015 Emmanuel Keller / QWAZR
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,92 +25,107 @@ import java.util.*;
 @JsonInclude(Include.NON_EMPTY)
 public class GraphNode {
 
-	public Map<String, Object> properties;
-	public Map<String, Set<Object>> edges;
+    public Map<String, Object> properties;
+    public Map<String, Set<Object>> edges;
 
-	public GraphNode() {
-		properties = null;
-		edges = null;
-	}
+    public GraphNode() {
+	properties = null;
+	edges = null;
+    }
 
-	@JsonIgnore
-	@XmlTransient
-	public boolean addProperty(String name, Object value) {
-		if (name == null || name.isEmpty() || value == null)
-			return false;
-		if (properties == null)
-			properties = new LinkedHashMap<String, Object>();
-		else if (properties.containsKey(name))
-			return false;
-		properties.put(name, value);
-		return true;
-	}
+    @JsonIgnore
+    @XmlTransient
+    public boolean addProperty(String name, Object value) {
+	if (name == null || name.isEmpty() || value == null)
+	    return false;
+	if (properties == null)
+	    properties = new LinkedHashMap<String, Object>();
+	else if (properties.containsKey(name))
+	    return false;
+	properties.put(name, value);
+	return true;
+    }
 
-	private Set<Object> getEdgeSet(String type) {
-		if (type == null || type.isEmpty())
-			return null;
-		if (edges == null)
-			edges = new LinkedHashMap<String, Set<Object>>();
-		Set<Object> nodeIdSet = edges.get(type);
-		if (nodeIdSet != null)
-			return nodeIdSet;
-		nodeIdSet = new TreeSet<Object>();
-		edges.put(type, nodeIdSet);
-		return nodeIdSet;
-	}
+    private Set<Object> getEdgeSet(String type) {
+	if (type == null || type.isEmpty())
+	    return null;
+	if (edges == null)
+	    edges = new LinkedHashMap<String, Set<Object>>();
+	Set<Object> nodeIdSet = edges.get(type);
+	if (nodeIdSet != null)
+	    return nodeIdSet;
+	nodeIdSet = new TreeSet<Object>();
+	edges.put(type, nodeIdSet);
+	return nodeIdSet;
+    }
 
-	@JsonIgnore
-	@XmlTransient
-	public boolean addEdge(String type, Object value) {
-		if (value == null)
-			return false;
-		if (value instanceof Collection)
-			return addEdges(type, (Collection<Object>) value);
-		Set<Object> nodeIdSet = getEdgeSet(type);
-		if (nodeIdSet == null)
-			return false;
-		nodeIdSet.add(value);
-		return true;
-	}
+    @JsonIgnore
+    @XmlTransient
+    public boolean addEdge(String type, Object value) {
+	if (value == null)
+	    return false;
+	if (value instanceof Collection)
+	    return addEdges(type, (Collection<Object>) value);
+	if (value.getClass().isArray())
+	    return addEdges(type, (Object[]) value);
+	Set<Object> nodeIdSet = getEdgeSet(type);
+	if (nodeIdSet == null)
+	    return false;
+	nodeIdSet.add(value);
+	return true;
+    }
 
-	@JsonIgnore
-	@XmlTransient
-	public boolean addEdges(String type, Collection<Object> values) {
-		if (values == null || values.isEmpty())
-			return false;
-		Set<Object> nodeIdSet = getEdgeSet(type);
-		if (nodeIdSet == null)
-			return false;
-		nodeIdSet.addAll(values);
-		return true;
-	}
+    @JsonIgnore
+    @XmlTransient
+    public boolean addEdges(String type, Collection<Object> values) {
+	if (values == null || values.isEmpty())
+	    return false;
+	Set<Object> nodeIdSet = getEdgeSet(type);
+	if (nodeIdSet == null)
+	    return false;
+	nodeIdSet.addAll(values);
+	return true;
+    }
 
-	@JsonIgnore
-	@XmlTransient
-	public boolean removeEdge(String type, String value) {
-		if (value == null || value.isEmpty())
-			return false;
-		if (edges == null)
-			return false;
-		if (type == null || type.isEmpty())
-			return false;
-		Set<Object> nodeIdSet = edges.get(type);
-		if (nodeIdSet == null)
-			return false;
-		return nodeIdSet.remove(value);
-	}
+    @JsonIgnore
+    @XmlTransient
+    public boolean addEdges(String type, Object[] values) {
+	if (values == null || values.length == 0)
+	    return false;
+	Set<Object> nodeIdSet = getEdgeSet(type);
+	if (nodeIdSet == null)
+	    return false;
+	for (Object value : values)
+	    nodeIdSet.add(value);
+	return true;
+    }
 
-	@JsonIgnore
-	@XmlTransient
-	public void add(GraphNode node) {
-		if (node == null)
-			return;
-		if (node.properties != null)
-			for (Map.Entry<String, Object> entry : node.properties.entrySet())
-				addProperty(entry.getKey(), entry.getValue());
-		if (node.edges != null)
-			for (Map.Entry<String, Set<Object>> entry : node.edges.entrySet())
-				addEdges(entry.getKey(), entry.getValue());
-	}
+    @JsonIgnore
+    @XmlTransient
+    public boolean removeEdge(String type, String value) {
+	if (value == null || value.isEmpty())
+	    return false;
+	if (edges == null)
+	    return false;
+	if (type == null || type.isEmpty())
+	    return false;
+	Set<Object> nodeIdSet = edges.get(type);
+	if (nodeIdSet == null)
+	    return false;
+	return nodeIdSet.remove(value);
+    }
+
+    @JsonIgnore
+    @XmlTransient
+    public void add(GraphNode node) {
+	if (node == null)
+	    return;
+	if (node.properties != null)
+	    for (Map.Entry<String, Object> entry : node.properties.entrySet())
+		addProperty(entry.getKey(), entry.getValue());
+	if (node.edges != null)
+	    for (Map.Entry<String, Set<Object>> entry : node.edges.entrySet())
+		addEdges(entry.getKey(), entry.getValue());
+    }
 
 }
