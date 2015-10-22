b1f409798f29f5b432e7adf58c39c20ca9e9f74d
Emmanuel Keller <ekeller@open-search-server.com>
2015-10-22 18:42:18 +0200

    Support of error handling using XML and JSON

--- qwazr-utils/src/main/java/com/qwazr/utils/server/ServletApplication.java
+++ qwazr-utils/src/main/java/com/qwazr/utils/server/ServletApplication.java
@@ -1,12 +1,12 @@
 /**
  * s * Copyright 2014-2015 Emmanuel Keller / QWAZR
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,22 +27,23 @@ import java.util.List;
  */
 public abstract class ServletApplication {
 
-    protected abstract List<ServletInfo> getServletInfos();
+	protected abstract List<ServletInfo> getServletInfos();
 
-    protected abstract SessionListener getSessionListener();
+	protected abstract SessionListener getSessionListener();
 
-    protected abstract String getContextPath();
+	protected abstract String getContextPath();
 
-    DeploymentInfo getDeploymentInfo() {
-	DeploymentInfo deploymentInfo = Servlets.deployment().setClassLoader(getClass().getClassLoader())
-		.setContextPath(getContextPath()).setDefaultEncoding(java.nio.charset.Charset.defaultCharset().name())
-		.setDeploymentName(getClass().getName() + getContextPath());
-	List<ServletInfo> servletInfos = getServletInfos();
-	if (servletInfos != null)
-	    deploymentInfo.addServlets(servletInfos);
-	SessionListener sessionListener = getSessionListener();
-	if (sessionListener != null)
-	    deploymentInfo.addSessionListener(sessionListener);
-	return deploymentInfo;
-    }
+	DeploymentInfo getDeploymentInfo() {
+		DeploymentInfo deploymentInfo = Servlets.deployment().setClassLoader(getClass().getClassLoader())
+						.setContextPath(getContextPath())
+						.setDefaultEncoding(java.nio.charset.Charset.defaultCharset().name())
+						.setDeploymentName(getClass().getName() + getContextPath());
+		List<ServletInfo> servletInfos = getServletInfos();
+		if (servletInfos != null)
+			deploymentInfo.addServlets(servletInfos);
+		SessionListener sessionListener = getSessionListener();
+		if (sessionListener != null)
+			deploymentInfo.addSessionListener(sessionListener);
+		return deploymentInfo;
+	}
 }
