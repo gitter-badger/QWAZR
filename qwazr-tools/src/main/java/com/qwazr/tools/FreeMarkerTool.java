13c5a9d302f7fe6b2aa7d12995cf3e4b1e074d24
Emmanuel Keller <ekeller@open-search-server.com>
2015-10-24 17:27:34 +0200

    Introduce schema (set of indexes)

--- qwazr-tools/src/main/java/com/qwazr/tools/FreeMarkerTool.java
+++ qwazr-tools/src/main/java/com/qwazr/tools/FreeMarkerTool.java
@@ -31,80 +31,80 @@ import java.util.Map;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class FreeMarkerTool extends AbstractTool implements TemplateLoader {
 
-    public final String output_encoding;
-    public final String default_encoding;
-    public final String default_content_type;
+	public final String output_encoding;
+	public final String default_encoding;
+	public final String default_content_type;
 
-    @JsonIgnore
-    protected Configuration cfg = null;
+	@JsonIgnore
+	protected Configuration cfg = null;
 
-    @JsonIgnore
-    protected File parentDir = null;
+	@JsonIgnore
+	protected File parentDir = null;
 
-    private final static String DEFAULT_CHARSET = "UTF-8";
-    private final static String DEFAULT_CONTENT_TYPE = "text/html";
+	private final static String DEFAULT_CHARSET = "UTF-8";
+	private final static String DEFAULT_CONTENT_TYPE = "text/html";
 
-    public FreeMarkerTool() {
-	output_encoding = null;
-	default_encoding = null;
-	default_content_type = null;
-    }
-
-    @Override
-    public void load(File parentDir) {
-	this.parentDir = parentDir;
-	cfg = new Configuration(Configuration.VERSION_2_3_22);
-	cfg.setTemplateLoader(this);
-	cfg.setOutputEncoding(output_encoding == null ? DEFAULT_CHARSET : output_encoding);
-	cfg.setDefaultEncoding(default_encoding == null ? DEFAULT_CHARSET : default_encoding);
-	cfg.setLocalizedLookup(false);
-	cfg.setTagSyntax(Configuration.AUTO_DETECT_TAG_SYNTAX);
-	cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-    }
-
-    @Override
-    public void unload() {
-	cfg.clearTemplateCache();
-    }
-
-    @Override
-    public Object findTemplateSource(String path) throws IOException {
-	File file = new File(parentDir, path);
-	return file.exists() && file.isFile() ? file : null;
-    }
-
-    @Override
-    public long getLastModified(Object templateSource) {
-	return ((File) templateSource).lastModified();
-    }
-
-    @Override
-    public Reader getReader(Object templateSource, String encoding) throws IOException {
-	return new FileReader((File) templateSource);
-    }
-
-    @Override
-    public void closeTemplateSource(Object templateSource) throws IOException {
-    }
-
-    public void template(String templatePath, Map<String, Object> dataModel, HttpServletResponse response)
-		    throws TemplateException, IOException {
-	if (response.getContentType() == null)
-	    response.setContentType(default_content_type == null ? DEFAULT_CONTENT_TYPE : default_content_type);
-	response.setCharacterEncoding(DEFAULT_CHARSET);
-	Template template = cfg.getTemplate(templatePath);
-	template.process(dataModel, response.getWriter());
-    }
-
-    public String template(String templatePath, Map<String, Object> dataModel) throws TemplateException, IOException {
-	Template template = cfg.getTemplate(templatePath);
-	StringWriter stringWriter = new StringWriter();
-	try {
-	    template.process(dataModel, stringWriter);
-	    return stringWriter.toString();
-	} finally {
-	    IOUtils.closeQuietly(stringWriter);
+	public FreeMarkerTool() {
+		output_encoding = null;
+		default_encoding = null;
+		default_content_type = null;
 	}
-    }
+
+	@Override
+	public void load(File parentDir) {
+		this.parentDir = parentDir;
+		cfg = new Configuration(Configuration.VERSION_2_3_22);
+		cfg.setTemplateLoader(this);
+		cfg.setOutputEncoding(output_encoding == null ? DEFAULT_CHARSET : output_encoding);
+		cfg.setDefaultEncoding(default_encoding == null ? DEFAULT_CHARSET : default_encoding);
+		cfg.setLocalizedLookup(false);
+		cfg.setTagSyntax(Configuration.AUTO_DETECT_TAG_SYNTAX);
+		cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+	}
+
+	@Override
+	public void unload() {
+		cfg.clearTemplateCache();
+	}
+
+	@Override
+	public Object findTemplateSource(String path) throws IOException {
+		File file = new File(parentDir, path);
+		return file.exists() && file.isFile() ? file : null;
+	}
+
+	@Override
+	public long getLastModified(Object templateSource) {
+		return ((File) templateSource).lastModified();
+	}
+
+	@Override
+	public Reader getReader(Object templateSource, String encoding) throws IOException {
+		return new FileReader((File) templateSource);
+	}
+
+	@Override
+	public void closeTemplateSource(Object templateSource) throws IOException {
+	}
+
+	public void template(String templatePath, Map<String, Object> dataModel, HttpServletResponse response)
+					throws TemplateException, IOException {
+		if (response.getContentType() == null)
+			response.setContentType(default_content_type == null ? DEFAULT_CONTENT_TYPE : default_content_type);
+		response.setCharacterEncoding(DEFAULT_CHARSET);
+		Template template = cfg.getTemplate(templatePath);
+		template.process(dataModel, response.getWriter());
+	}
+
+	public String template(String templatePath, Map<String, Object> dataModel) throws TemplateException, IOException {
+		Template template = cfg.getTemplate(templatePath);
+		StringWriter stringWriter = new StringWriter();
+		try {
+			template.process(dataModel, stringWriter);
+			return stringWriter.toString();
+		} finally {
+			IOUtils.closeQuietly(stringWriter);
+		}
+	}
 
 }
