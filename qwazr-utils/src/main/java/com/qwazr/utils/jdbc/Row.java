b46151eea813f9b6c2fd2ae6a537006d7b3afe43
Emmanuel Keller <ekeller@open-search-server.com>
2015-12-07 20:55:06 +0100

    Add row iterator to JDBC

--- qwazr-utils/src/main/java/com/qwazr/utils/jdbc/Row.java
+++ qwazr-utils/src/main/java/com/qwazr/utils/jdbc/Row.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2014-2015 Emmanuel Keller / QWAZR
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,8 +15,11 @@
  */
 package com.qwazr.utils.jdbc;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.AbstractMap;
 import java.util.Collection;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -25,55 +28,65 @@ import java.util.Set;
  */
 public class Row extends AbstractMap<String, Object> {
 
-    private final RowSet rowSet;
+	final Map<String, Integer> columnMap;
 
-    Row(RowSet rowSet) {
-	this.rowSet = rowSet;
-    }
+	final Object[] columns;
 
-    @Override
-    public Set<Entry<String, Object>> entrySet() {
-	return rowSet;
-    }
+	Row(Map<String, Integer> columnMap) {
+		this.columnMap = columnMap;
+		this.columns = new Object[columnMap.size()];
+	}
 
-    /**
-     * @param columnNumber
-     *            the number of the column
-     * @return the value for the give column
-     */
-    final public Object get(int columnNumber) {
-	Object col = rowSet.columns[columnNumber];
-	if (col == null)
-	    return null;
-	return col;
-    }
+	Row(Map<String, Integer> columnMap, ResultSet rs) throws SQLException {
+		this(columnMap);
+		for (Map.Entry<String, Integer> entry : columnMap.entrySet()) {
+			int columnIndex = entry.getValue();
+			Object object = rs.getObject(columnIndex + 1);
+			columns[columnIndex] = object;
+		}
+	}
 
-    /**
-     * @param label
-     *            the label of the column
-     * @return the value for the given column label
-     */
-    final public Object get(Object label) {
-	Integer colNumber = rowSet.columnMap.get(label.toString());
-	if (colNumber == null)
-	    return null;
-	if (colNumber > rowSet.columns.length)
-	    return null;
-	return rowSet.columns[colNumber];
-    }
+	/**
+	 * @param columnNumber the number of the column
+	 * @return the value for the give column
+	 */
+	final public Object get(int columnNumber) {
+		Object col = columns[columnNumber];
+		if (col == null)
+			return null;
+		return col;
+	}
 
-    @Override
-    public boolean containsValue(Object value) {
-	if (rowSet.columns == null)
-	    return false;
-	for (Object col : rowSet.columns)
-	    if (col == value)
-		return true;
-	return false;
-    }
+	/**
+	 * @param label the label of the column
+	 * @return the value for the given column label
+	 */
+	final public Object get(Object label) {
+		Integer colNumber = columnMap.get(label.toString());
+		if (colNumber == null)
+			return null;
+		if (colNumber > columns.length)
+			return null;
+		return columns[colNumber];
+	}
 
-    public Collection<String> getColumns() {
-	return rowSet.columnMap.keySet();
-    }
+	@Override
+	public Set<Entry<String, Object>> entrySet() {
+		throw new UnsupportedOperationException("This map cannot export its entry set");
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		if (columns == null)
+			return false;
+		for (Object col : columns)
+			if (col == value)
+				return true;
+		return false;
+	}
+
+	public Collection<String> getColumns() {
+		return columnMap.keySet();
+	}
 
 }
--- qwazr-utils/src/main/java/com/qwazr/utils/jdbc/RowIterator.java
+++ qwazr-utils/src/main/java/com/qwazr/utils/jdbc/RowIterator.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2014-2015 Emmanuel Keller / QWAZR
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,28 +15,47 @@
  */
 package com.qwazr.utils.jdbc;
 
-import java.util.AbstractMap;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.Map;
 
-class RowIterator implements Iterator<Map.Entry<String, Object>> {
+class RowIterator implements Iterator<Row> {
 
-    private final Iterator<Map.Entry<String, Integer>> iterator;
-    private final Object[] columns;
+	private final Map<String, Integer> columnMap;
 
-    RowIterator(Iterator<Map.Entry<String, Integer>> iterator, Object[] columns) {
-	this.iterator = iterator;
-	this.columns = columns;
-    }
+	private final ResultSet resultSet;
 
-    @Override
-    public boolean hasNext() {
-	return iterator.hasNext();
-    }
+	private Boolean next;
 
-    @Override
-    public Map.Entry<String, Object> next() {
-	Map.Entry<String, Integer> entry = iterator.next();
-	return new AbstractMap.SimpleEntry(entry.getKey(), columns[entry.getValue()]);
-    }
+	RowIterator(Map<String, Integer> columnMap, ResultSet resultSet) throws SQLException {
+		this.columnMap = columnMap;
+		this.resultSet = resultSet;
+		// If the result set is null, "next" is immedialy set to false (and will not be evaluated again)
+		this.next = resultSet == null ? false : null;
+	}
+
+	@Override
+	public boolean hasNext() {
+		if (next != null)
+			return next;
+		try {
+			next = resultSet.next();
+			return next;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public Row next() {
+		try {
+			if (!hasNext())
+				return null;
+			next = null; // Set to null, that way "Next" will be reevaluated
+			return new Row(columnMap, resultSet);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
