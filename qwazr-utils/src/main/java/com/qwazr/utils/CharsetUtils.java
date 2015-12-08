a415c4f85c2a2382ebd45138ee074968800850b1
Emmanuel Keller <ekeller@open-search-server.com>
2015-12-08 21:37:29 +0100

    Few improvements

--- qwazr-utils/src/main/java/com/qwazr/utils/CharsetUtils.java
+++ qwazr-utils/src/main/java/com/qwazr/utils/CharsetUtils.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2014-2015 Emmanuel Keller / QWAZR
- * <p/>
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p/>
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,6 +15,9 @@
  */
 package com.qwazr.utils;
 
+import com.ibm.icu.text.CharsetDetector;
+import com.ibm.icu.text.CharsetMatch;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
@@ -22,53 +25,50 @@ import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetEncoder;
 
-import com.ibm.icu.text.CharsetDetector;
-import com.ibm.icu.text.CharsetMatch;
-
 public class CharsetUtils {
 
-    public final static String charsetDetector(InputStream inputStream) throws IOException {
-	CharsetDetector detector = new CharsetDetector();
-	detector.setText(inputStream);
-	CharsetMatch match = detector.detect();
-	if (match == null)
-	    return null;
-	return match.getName();
-    }
-
-    public final static String charsetDetector(byte[] bytes) {
-	CharsetDetector detector = new CharsetDetector();
-	detector.setText(bytes);
-	CharsetMatch match = detector.detect();
-	if (match == null)
-	    return null;
-	return match.getName();
-    }
-
-    public final static Charset CharsetUTF8 = Charset.forName("UTF-8");
-
-    public final static CharsetEncoder newUTF8Encoder() {
-	synchronized (CharsetUTF8) {
-	    return CharsetUTF8.newEncoder();
+	public final static String charsetDetector(InputStream inputStream) throws IOException {
+		CharsetDetector detector = new CharsetDetector();
+		detector.setText(inputStream);
+		CharsetMatch match = detector.detect();
+		if (match == null)
+			return null;
+		return match.getName();
 	}
-    }
 
-    public final static byte[] encodeUtf8(String string) {
-	if (string == null)
-	    return null;
-	return string.getBytes(CharsetUTF8);
-    }
+	public final static String charsetDetector(byte[] bytes) {
+		CharsetDetector detector = new CharsetDetector();
+		detector.setText(bytes);
+		CharsetMatch match = detector.detect();
+		if (match == null)
+			return null;
+		return match.getName();
+	}
 
-    public final static String decodeUtf8(byte[] bytes) {
-	if (bytes == null)
-	    return null;
-	return new String(bytes, CharsetUTF8);
-    }
+	public final static Charset CharsetUTF8 = Charset.forName("UTF-8");
 
-    public final static String decodeUtf8(ByteBuffer bytes) throws CharacterCodingException {
-	if (bytes == null)
-	    return null;
-	return new String(CharsetUTF8.newDecoder().decode(bytes).array());
-    }
+	public final static CharsetEncoder newUTF8Encoder() {
+		synchronized (CharsetUTF8) {
+			return CharsetUTF8.newEncoder();
+		}
+	}
+
+	public final static byte[] encodeUtf8(String string) {
+		if (string == null)
+			return null;
+		return string.getBytes(CharsetUTF8);
+	}
+
+	public final static String decodeUtf8(byte[] bytes) {
+		if (bytes == null)
+			return null;
+		return new String(bytes, CharsetUTF8);
+	}
+
+	public final static String decodeUtf8(ByteBuffer bytes) throws CharacterCodingException {
+		if (bytes == null)
+			return null;
+		return new String(CharsetUTF8.newDecoder().decode(bytes).array());
+	}
 
 }
