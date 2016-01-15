e4aecadde148f38fea0c4f551cfd4003addea911
Emmanuel Keller <ekeller@open-search-server.com>
2016-01-15 13:38:47 +0100

    Add more log info (related to groups)

--- qwazr-utils/src/main/java/com/qwazr/utils/ArrayUtils.java
+++ qwazr-utils/src/main/java/com/qwazr/utils/ArrayUtils.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2015-2016 Emmanuel Keller / QWAZR
- * <p/>
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p/>
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,53 +15,60 @@
  */
 package com.qwazr.utils;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 public class ArrayUtils extends org.apache.commons.lang3.ArrayUtils {
 
-    public static boolean startsWith(final byte[] array, final byte[] prefix) {
-	if (array == null)
-	    return false;
-	if (prefix == null)
-	    return false;
-	if (prefix.length > array.length)
-	    return false;
-	for (int i = 0; i < prefix.length; i++)
-	    if (array[i] != prefix[i])
-		return false;
-	return true;
-    }
+	public static boolean startsWith(final byte[] array, final byte[] prefix) {
+		if (array == null)
+			return false;
+		if (prefix == null)
+			return false;
+		if (prefix.length > array.length)
+			return false;
+		for (int i = 0; i < prefix.length; i++)
+			if (array[i] != prefix[i])
+				return false;
+		return true;
+	}
 
-    public static double[] toPrimitiveDouble(Collection<Double> collection) {
-	double[] array = new double[collection.size()];
-	int i = 0;
-	for (double val : collection)
-	    array[i++] = val;
-	return array;
-    }
+	public static double[] toPrimitiveDouble(Collection<Double> collection) {
+		double[] array = new double[collection.size()];
+		int i = 0;
+		for (double val : collection)
+			array[i++] = val;
+		return array;
+	}
 
-    public static int[] toPrimitiveInt(Collection<Integer> collection) {
-	int[] array = new int[collection.size()];
-	int i = 0;
-	for (int val : collection)
-	    array[i++] = val;
-	return array;
-    }
+	public static int[] toPrimitiveInt(Collection<Integer> collection) {
+		int[] array = new int[collection.size()];
+		int i = 0;
+		for (int val : collection)
+			array[i++] = val;
+		return array;
+	}
 
-    public static long[] toPrimitiveLong(Collection<Long> collection) {
-	long[] array = new long[collection.size()];
-	int i = 0;
-	for (long val : collection)
-	    array[i++] = val;
-	return array;
-    }
+	public static long[] toPrimitiveLong(Collection<Long> collection) {
+		long[] array = new long[collection.size()];
+		int i = 0;
+		for (long val : collection)
+			array[i++] = val;
+		return array;
+	}
 
-    public static float[] toPrimitiveFloat(Collection<Float> collection) {
-	float[] array = new float[collection.size()];
-	int i = 0;
-	for (float val : collection)
-	    array[i++] = val;
-	return array;
-    }
+	public static float[] toPrimitiveFloat(Collection<Float> collection) {
+		float[] array = new float[collection.size()];
+		int i = 0;
+		for (float val : collection)
+			array[i++] = val;
+		return array;
+	}
+
+	public static String prettyPrint(Collection<?> collection) {
+		if (collection == null)
+			return null;
+		return Arrays.toString(collection.toArray());
+	}
 
 }
