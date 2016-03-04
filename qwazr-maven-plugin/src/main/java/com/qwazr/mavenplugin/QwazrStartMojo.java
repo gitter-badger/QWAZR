02c5db2d96f0e1e294babdfb89822176f52d5648
Emmanuel Keller <ekeller@open-search-server.com>
2016-03-04 18:04:51 +0100

    Add support of services in daemon mode

--- qwazr-maven-plugin/src/main/java/com/qwazr/mavenplugin/QwazrStartMojo.java
+++ qwazr-maven-plugin/src/main/java/com/qwazr/mavenplugin/QwazrStartMojo.java
@@ -172,6 +172,9 @@ public class QwazrStartMojo extends AbstractMojo {
 			if (groups != null && !groups.isEmpty())
 				parameters.put(QwazrConfiguration.VariablesEnum.QWAZR_GROUPS.name(), StringUtils.join(groups, ","));
 
+			if (services != null && !services.isEmpty())
+				parameters.put(QwazrConfiguration.VariablesEnum.QWAZR_SERVICES.name(), StringUtils.join(services, ","));
+
 			final String className = Qwazr.class.getCanonicalName();
 			final ProcessBuilder builder = new ProcessBuilder(javaBinFile.getCanonicalPath(), "-Dfile.encoding=UTF-8",
 							className);
